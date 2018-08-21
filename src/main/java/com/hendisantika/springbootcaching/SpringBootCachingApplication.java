e2454801669c0a78264fa398181c04e3a19d4ae6
Hendi Santika <hendisantika@gmail.com>
2018-08-21 08:33:20 +0700

    #6 - Modify Main App

--- src/main/java/com/hendisantika/springbootcaching/SpringBootCachingApplication.java
+++ src/main/java/com/hendisantika/springbootcaching/SpringBootCachingApplication.java
@@ -1,12 +1,34 @@
 package com.hendisantika.springbootcaching;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-public class SpringBootCachingApplication {
+@EnableCaching
+public class SpringBootCachingApplication implements CommandLineRunner {
+
+    private static final Logger logger = LoggerFactory.getLogger(SpringBootCachingApplication.class);
+
+    @Autowired
+    BookRepository bookRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootCachingApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
+        logger.info(".... Fetching books");
+        logger.info("isbn-1234 -->" + bookRepository.getByIsbn("isbn-1234"));
+        logger.info("isbn-4567 -->" + bookRepository.getByIsbn("isbn-4567"));
+        logger.info("isbn-1234 -->" + bookRepository.getByIsbn("isbn-1234"));
+        logger.info("isbn-4567 -->" + bookRepository.getByIsbn("isbn-4567"));
+        logger.info("isbn-1234 -->" + bookRepository.getByIsbn("isbn-1234"));
+        logger.info("isbn-1234 -->" + bookRepository.getByIsbn("isbn-1234"));
     }
 }
