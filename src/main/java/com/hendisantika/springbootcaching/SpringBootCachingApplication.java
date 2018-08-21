3333cadba6dc0f5c615cf7bd5858f235d2e42267
Hendi Santika <hendisantika@gmail.com>
2018-08-21 08:16:47 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootcaching/SpringBootCachingApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springbootcaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCachingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCachingApplication.class, args);
+    }
+}
